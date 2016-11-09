af2e10b4e6865217fb5956237e50210520c683b6
KoreaChanghoon <gravity7508@gmail.com>
2016-11-09 16:43:59 +0900

    Login, Signup Form
    Changhoon - Login, Signup Form

---
+++ NurseDutyManager/LoginForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SignupForm f2 = new SignupForm();
+            f2.Owner = this;   //Form1 지정
+            f2.Show();         //Form2 열기
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
