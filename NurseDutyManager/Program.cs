d8e6a7dc0745614cd40aba139f44279dda9b70b7
KoreaChanghoon <gravity7508@gmail.com>
2016-11-21 11:32:24 +0900

    Find ID,PW

--- NurseDutyManager/Program.cs
+++ NurseDutyManager/Program.cs
@@ -4,26 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace NurseDutyManager
+namespace WindowsFormsApplication6
 {
-	static class Program
-	{
-		/// <summary>
-		/// 해당 응용 프로그램의 주 진입점입니다.
-		/// </summary>
-		[STAThread]
-		static void Main()
-		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new ApplyOff());
-			Application.Run(new ChiefMenuForm());
-			Application.Run(new DutyList());
-			Application.Run(new LoginForm());
-			Application.Run(new ManageMemberForm());
-			Application.Run(new NightShiftForm());
-			Application.Run(new OffOptionForm());
-			Application.Run(new SignupForm());
-		}
-	}
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Find_Info());
+        }
+    }
 }
