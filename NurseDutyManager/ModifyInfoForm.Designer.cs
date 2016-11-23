6017c119bfd9ba2d1686b02e6d430cba563b16a0
KoreaChanghoon <gravity7508@gmail.com>
2016-11-24 03:56:24 +0900

    ModifyInfoForm, SignupForm coding

--- NurseDutyManager/ModifyInfoForm.Designer.cs
+++ NurseDutyManager/ModifyInfoForm.Designer.cs
@@ -178,6 +178,7 @@
             this.button1.TabIndex = 15;
             this.button1.Text = "수정완료";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
--- NurseDutyManager/ModifyInfoForm.cs
+++ NurseDutyManager/ModifyInfoForm.cs
@@ -12,6 +12,9 @@ namespace NurseDutyManager
 {
     public partial class ModifyInfoForm : Form
     {
+        ClientSocket clientsocket;
+        Nurse currentNurse;
+
         public ModifyInfoForm()
         {
             InitializeComponent();
@@ -19,6 +22,29 @@ namespace NurseDutyManager
             comboBox1.Items.Add("일반간호사");
             comboBox2.Items.Add("남");
             comboBox2.Items.Add("여");
+        }
+        public ModifyInfoForm(ClientSocket _clientsocket, string _id)
+        {
+            clientsocket = _clientsocket;
+            currentNurse = clientsocket.getNurse(_id);
+
+            if (comboBox1.SelectedItem.Equals("수간호사"))
+                currentNurse.IsChiefNurse = true;
+            else
+                currentNurse.IsChiefNurse = false;
+            currentNurse.Name = textBox1.Text;
+            currentNurse.Password = textBox2.Text;
+            if (comboBox2.SelectedItem.Equals("남"))
+                currentNurse.Sex = SEX.Male;
+            else
+                currentNurse.Sex = SEX.Female;
+            currentNurse.LicenseNum = textBox4.Text;
+            currentNurse.PhoneNum = textBox5.Text;
+            currentNurse.Group = GROUP.Group3;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("수정 완료");
         }
     }
 }
