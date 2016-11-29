aa2f4b18ace756f169d2e95c1a31aef966f28a59
DGU-LSW <dgulsw@gmail.com>
2016-11-30 05:33:58 +0900

    for Schedule

--- NurseDutyManager/DaySchedule.cs
+++ NurseDutyManager/DaySchedule.cs
@@ -8,5 +8,49 @@ namespace NurseDutyManager
 {
     public class DaySchedule
     {
+        WORK[] work;
+        DayOfWeek week;
+        public DaySchedule() { }
+        public DaySchedule(int numNurse, int  _week):this()
+        {
+            work = new WORK[numNurse];
+            for(int i = 0; i < work.Count(); i++)
+            {
+                work[i] = WORK.V;
+            }
+            _week %= 7;
+            switch (_week)
+            {
+                case 0:
+                    week = DayOfWeek.Monday;
+                    break;
+                case 1:
+                    week = DayOfWeek.Tuesday;
+                    break;
+                case 2:
+                    week = DayOfWeek.Wednesday;
+                    break;
+                case 3:
+                    week = DayOfWeek.Thursday;
+                    break;
+                case 4:
+                    week = DayOfWeek.Friday;
+                    break;
+                case 5:
+                    week = DayOfWeek.Saturday;
+                    break;
+                case 6:
+                    week = DayOfWeek.Sunday;
+                    break;
+            }
+        }
+        public void setWork(int _index, WORK _work)
+        {
+            work[_index] = _work;
+        }
+        public WORK getWork(int _indexNurse)
+        {
+            return work[_indexNurse];
+        }
     }
 }
--- NurseDutyManager/Scheduler.cs
+++ NurseDutyManager/Scheduler.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace NurseDutyManager
 {
+    public enum WORK
+    {
+        D,  //day
+        E,  //even
+        N,  //night
+        O,  //off
+        V   //vacum
+    };
     public class Scheduler
     {
+        ClientSocket clientsocket;
+
+        private MonthSchedule makeSchedule()
+        {
+            MonthSchedule result = null;
+
+            List<Nurse> nurseList = null;
+            List<Off> offList = null;
+            Option option = null;
+
+            
+
+            return result;
+        }
     }
 }
