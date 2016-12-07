83dfec85a8fb60ff7c03cfc488e3cdae7f720492
jeheeim <jeheeim@gmail.com>
2016-12-08 00:44:27 +0900

    index fic

--- NurseDutyManager/SignupForm.Designer.cs
+++ NurseDutyManager/SignupForm.Designer.cs
@@ -33,229 +33,229 @@ namespace NurseDutyManager
         /// </summary>
         private void InitializeComponent()
         {
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.textBoxID = new System.Windows.Forms.TextBox();
-            this.textBoxPW2 = new System.Windows.Forms.TextBox();
-            this.textBoxPW = new System.Windows.Forms.TextBox();
-            this.textBoxLicenseNum = new System.Windows.Forms.TextBox();
-            this.textBoxPhone = new System.Windows.Forms.TextBox();
-            this.textBoxName = new System.Windows.Forms.TextBox();
-            this.buttonComplete = new System.Windows.Forms.Button();
-            this.buttonCancel = new System.Windows.Forms.Button();
-            this.buttonConformID = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.comboBoxNurse = new System.Windows.Forms.ComboBox();
-            this.comboBoxSex = new System.Windows.Forms.ComboBox();
-            this.label9 = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 34);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(29, 12);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "이름";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 61);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(41, 12);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "아이디";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 88);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(53, 12);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "비밀번호";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(12, 115);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(77, 12);
-            this.label6.TabIndex = 5;
-            this.label6.Text = "비밀번호확인";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(12, 168);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(53, 12);
-            this.label7.TabIndex = 6;
-            this.label7.Text = "면허번호";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(12, 195);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(41, 12);
-            this.label8.TabIndex = 7;
-            this.label8.Text = "휴대폰";
-            // 
-            // textBoxID
-            // 
-            this.textBoxID.Location = new System.Drawing.Point(90, 58);
-            this.textBoxID.Name = "textBoxID";
-            this.textBoxID.Size = new System.Drawing.Size(100, 21);
-            this.textBoxID.TabIndex = 10;
-            this.textBoxID.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // textBoxPW2
-            // 
-            this.textBoxPW2.Location = new System.Drawing.Point(90, 112);
-            this.textBoxPW2.Name = "textBoxPW2";
-            this.textBoxPW2.Size = new System.Drawing.Size(100, 21);
-            this.textBoxPW2.TabIndex = 11;
-            // 
-            // textBoxPW
-            // 
-            this.textBoxPW.Location = new System.Drawing.Point(90, 85);
-            this.textBoxPW.Name = "textBoxPW";
-            this.textBoxPW.Size = new System.Drawing.Size(100, 21);
-            this.textBoxPW.TabIndex = 12;
-            // 
-            // textBoxLicenseNum
-            // 
-            this.textBoxLicenseNum.Location = new System.Drawing.Point(90, 165);
-            this.textBoxLicenseNum.Name = "textBoxLicenseNum";
-            this.textBoxLicenseNum.Size = new System.Drawing.Size(100, 21);
-            this.textBoxLicenseNum.TabIndex = 13;
-            // 
-            // textBoxPhone
-            // 
-            this.textBoxPhone.Location = new System.Drawing.Point(90, 192);
-            this.textBoxPhone.Name = "textBoxPhone";
-            this.textBoxPhone.Size = new System.Drawing.Size(100, 21);
-            this.textBoxPhone.TabIndex = 14;
-            // 
-            // textBoxName
-            // 
-            this.textBoxName.Location = new System.Drawing.Point(90, 31);
-            this.textBoxName.Name = "textBoxName";
-            this.textBoxName.Size = new System.Drawing.Size(100, 21);
-            this.textBoxName.TabIndex = 15;
-            // 
-            // buttonComplete
-            // 
-            this.buttonComplete.Location = new System.Drawing.Point(61, 219);
-            this.buttonComplete.Name = "buttonComplete";
-            this.buttonComplete.Size = new System.Drawing.Size(75, 23);
-            this.buttonComplete.TabIndex = 16;
-            this.buttonComplete.Text = "가입완료";
-            this.buttonComplete.UseVisualStyleBackColor = true;
-            this.buttonComplete.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // buttonCancel
-            // 
-            this.buttonCancel.Location = new System.Drawing.Point(143, 219);
-            this.buttonCancel.Name = "buttonCancel";
-            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
-            this.buttonCancel.TabIndex = 17;
-            this.buttonCancel.Text = "취소";
-            this.buttonCancel.UseVisualStyleBackColor = true;
-            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
-            // 
-            // buttonConformID
-            // 
-            this.buttonConformID.Location = new System.Drawing.Point(197, 56);
-            this.buttonConformID.Name = "buttonConformID";
-            this.buttonConformID.Size = new System.Drawing.Size(75, 23);
-            this.buttonConformID.TabIndex = 18;
-            this.buttonConformID.Text = "중복확인";
-            this.buttonConformID.UseVisualStyleBackColor = true;
-            this.buttonConformID.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(195, 195);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 12);
-            this.label1.TabIndex = 19;
-            this.label1.Text = "(- 제외)";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 142);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(29, 12);
-            this.label2.TabIndex = 20;
-            this.label2.Text = "성별";
-            // 
-            // comboBoxNurse
-            // 
-            this.comboBoxNurse.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBoxNurse.FormattingEnabled = true;
-            this.comboBoxNurse.Location = new System.Drawing.Point(90, 5);
-            this.comboBoxNurse.Name = "comboBoxNurse";
-            this.comboBoxNurse.Size = new System.Drawing.Size(100, 20);
-            this.comboBoxNurse.TabIndex = 23;
-            // 
-            // comboBoxSex
-            // 
-            this.comboBoxSex.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBoxSex.FormattingEnabled = true;
-            this.comboBoxSex.Location = new System.Drawing.Point(90, 139);
-            this.comboBoxSex.Name = "comboBoxSex";
-            this.comboBoxSex.Size = new System.Drawing.Size(100, 20);
-            this.comboBoxSex.TabIndex = 24;
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(15, 8);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(65, 12);
-            this.label9.TabIndex = 25;
-            this.label9.Text = "간호사유형";
-            // 
-            // SignupForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.comboBoxSex);
-            this.Controls.Add(this.comboBoxNurse);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.buttonConformID);
-            this.Controls.Add(this.buttonCancel);
-            this.Controls.Add(this.buttonComplete);
-            this.Controls.Add(this.textBoxName);
-            this.Controls.Add(this.textBoxPhone);
-            this.Controls.Add(this.textBoxLicenseNum);
-            this.Controls.Add(this.textBoxPW);
-            this.Controls.Add(this.textBoxPW2);
-            this.Controls.Add(this.textBoxID);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Name = "SignupForm";
-            this.Text = "회원가입";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.textBoxID = new System.Windows.Forms.TextBox();
+			this.textBoxPW2 = new System.Windows.Forms.TextBox();
+			this.textBoxPW = new System.Windows.Forms.TextBox();
+			this.textBoxLicenseNum = new System.Windows.Forms.TextBox();
+			this.textBoxPhone = new System.Windows.Forms.TextBox();
+			this.textBoxName = new System.Windows.Forms.TextBox();
+			this.buttonComplete = new System.Windows.Forms.Button();
+			this.buttonCancel = new System.Windows.Forms.Button();
+			this.buttonConformID = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.comboBoxNurse = new System.Windows.Forms.ComboBox();
+			this.comboBoxSex = new System.Windows.Forms.ComboBox();
+			this.label9 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(12, 34);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(29, 12);
+			this.label3.TabIndex = 12;
+			this.label3.Text = "이름";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(12, 61);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(41, 12);
+			this.label4.TabIndex = 13;
+			this.label4.Text = "아이디";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(12, 88);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(53, 12);
+			this.label5.TabIndex = 14;
+			this.label5.Text = "비밀번호";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(12, 115);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(77, 12);
+			this.label6.TabIndex = 15;
+			this.label6.Text = "비밀번호확인";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(12, 168);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(53, 12);
+			this.label7.TabIndex = 17;
+			this.label7.Text = "면허번호";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(12, 195);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(41, 12);
+			this.label8.TabIndex = 18;
+			this.label8.Text = "휴대폰";
+			// 
+			// textBoxID
+			// 
+			this.textBoxID.Location = new System.Drawing.Point(90, 58);
+			this.textBoxID.Name = "textBoxID";
+			this.textBoxID.Size = new System.Drawing.Size(100, 21);
+			this.textBoxID.TabIndex = 3;
+			this.textBoxID.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// textBoxPW2
+			// 
+			this.textBoxPW2.Location = new System.Drawing.Point(90, 112);
+			this.textBoxPW2.Name = "textBoxPW2";
+			this.textBoxPW2.Size = new System.Drawing.Size(100, 21);
+			this.textBoxPW2.TabIndex = 6;
+			// 
+			// textBoxPW
+			// 
+			this.textBoxPW.Location = new System.Drawing.Point(90, 85);
+			this.textBoxPW.Name = "textBoxPW";
+			this.textBoxPW.Size = new System.Drawing.Size(100, 21);
+			this.textBoxPW.TabIndex = 5;
+			// 
+			// textBoxLicenseNum
+			// 
+			this.textBoxLicenseNum.Location = new System.Drawing.Point(90, 165);
+			this.textBoxLicenseNum.Name = "textBoxLicenseNum";
+			this.textBoxLicenseNum.Size = new System.Drawing.Size(100, 21);
+			this.textBoxLicenseNum.TabIndex = 8;
+			// 
+			// textBoxPhone
+			// 
+			this.textBoxPhone.Location = new System.Drawing.Point(90, 192);
+			this.textBoxPhone.Name = "textBoxPhone";
+			this.textBoxPhone.Size = new System.Drawing.Size(100, 21);
+			this.textBoxPhone.TabIndex = 9;
+			// 
+			// textBoxName
+			// 
+			this.textBoxName.Location = new System.Drawing.Point(90, 31);
+			this.textBoxName.Name = "textBoxName";
+			this.textBoxName.Size = new System.Drawing.Size(100, 21);
+			this.textBoxName.TabIndex = 2;
+			// 
+			// buttonComplete
+			// 
+			this.buttonComplete.Location = new System.Drawing.Point(61, 219);
+			this.buttonComplete.Name = "buttonComplete";
+			this.buttonComplete.Size = new System.Drawing.Size(75, 23);
+			this.buttonComplete.TabIndex = 10;
+			this.buttonComplete.Text = "가입완료";
+			this.buttonComplete.UseVisualStyleBackColor = true;
+			this.buttonComplete.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// buttonCancel
+			// 
+			this.buttonCancel.Location = new System.Drawing.Point(143, 219);
+			this.buttonCancel.Name = "buttonCancel";
+			this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+			this.buttonCancel.TabIndex = 11;
+			this.buttonCancel.Text = "취소";
+			this.buttonCancel.UseVisualStyleBackColor = true;
+			this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+			// 
+			// buttonConformID
+			// 
+			this.buttonConformID.Location = new System.Drawing.Point(197, 56);
+			this.buttonConformID.Name = "buttonConformID";
+			this.buttonConformID.Size = new System.Drawing.Size(75, 23);
+			this.buttonConformID.TabIndex = 4;
+			this.buttonConformID.Text = "중복확인";
+			this.buttonConformID.UseVisualStyleBackColor = true;
+			this.buttonConformID.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(195, 195);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(49, 12);
+			this.label1.TabIndex = 19;
+			this.label1.Text = "(- 제외)";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 142);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(29, 12);
+			this.label2.TabIndex = 16;
+			this.label2.Text = "성별";
+			// 
+			// comboBoxNurse
+			// 
+			this.comboBoxNurse.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBoxNurse.FormattingEnabled = true;
+			this.comboBoxNurse.Location = new System.Drawing.Point(90, 5);
+			this.comboBoxNurse.Name = "comboBoxNurse";
+			this.comboBoxNurse.Size = new System.Drawing.Size(100, 20);
+			this.comboBoxNurse.TabIndex = 1;
+			// 
+			// comboBoxSex
+			// 
+			this.comboBoxSex.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBoxSex.FormattingEnabled = true;
+			this.comboBoxSex.Location = new System.Drawing.Point(90, 139);
+			this.comboBoxSex.Name = "comboBoxSex";
+			this.comboBoxSex.Size = new System.Drawing.Size(100, 20);
+			this.comboBoxSex.TabIndex = 7;
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(15, 8);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(65, 12);
+			this.label9.TabIndex = 25;
+			this.label9.Text = "간호사유형";
+			// 
+			// SignupForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.comboBoxSex);
+			this.Controls.Add(this.comboBoxNurse);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.buttonConformID);
+			this.Controls.Add(this.buttonCancel);
+			this.Controls.Add(this.buttonComplete);
+			this.Controls.Add(this.textBoxName);
+			this.Controls.Add(this.textBoxPhone);
+			this.Controls.Add(this.textBoxLicenseNum);
+			this.Controls.Add(this.textBoxPW);
+			this.Controls.Add(this.textBoxPW2);
+			this.Controls.Add(this.textBoxID);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Name = "SignupForm";
+			this.Text = "회원가입";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
